a309d814827c7ac74338df84db5a4510b93a31b8
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-09-08 10:26:13 +0300

    master добавил плагин autosave

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -229,6 +229,43 @@ require("lazy").setup({
   {
     "jake-stewart/multicursor.nvim",
     branch = "1.0",
+  },
+  {
+    "Pocco81/auto-save.nvim",
+    event = "VeryLazy",
+    config = function()
+      require('auto-save').setup({
+        enabled = true,
+        execution_message = {
+          message = function()
+            return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+          end,
+          dim = 0.18, 
+          cleaning_interval = 1000, 
+        },
+        trigger_events = {"InsertLeave", "TextChanged"},
+        condition = function(buf)
+          local fn = vim.fn
+          local utils = require("auto-save.utils.data")
+
+          if
+            fn.getbufvar(buf, "&modifiable") == 1 and
+            utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+            return true -- met condition(s), can save
+          end
+          return false -- can't save
+        end,
+        write_all_buffers = false, -- write all buffers when the current one meets `condition`
+        debounce_delay = 135, -- saves the file at most every `debounce_delay` milliseconds
+        callbacks = { -- functions to be executed at different intervals
+          enabling = nil, -- ran when enabling auto-save
+          disabling = nil, -- ran when disabling auto-save
+          before_asserting_save = nil, -- ran before checking `condition`
+          before_saving = nil, -- ran before doing the actual save
+          after_saving = nil -- ran after doing the actual save
+        }
+      })
+    end,
   }
 })
 vim.g.highlightedyank_highlight_duration = 300
