2218562d8bdea89bc4f33b61bdb65337c931e705
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-09-10 21:38:15 +0300

    master запилил nvimtree + пытаюсь подключить русский язык блять

---
+++ lua/neotree.lua
@@ -0,0 +1,9 @@
+vim.keymap.set("n", "<A-1>", "<Cmd>Neotree toggle<CR>", {
+  desc = "Toggle Neo-Tree (float)"
+})
+vim.keymap.set("n", "<A-2>", "<Cmd>Neotree buffers<CR>", {
+  desc = "Toggle Neo-Tree (float)"
+})
+vim.keymap.set("n", "<A-3>", "<Cmd>Neotree git_status<CR>", {
+  desc = "Toggle Neo-Tree (float)"
+})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -214,7 +214,18 @@ require("lazy").setup({
       })
     end,
   },
-  { "chrisgrieser/nvim-spider", lazy = true },
+  {
+    "theHamsta/nvim_rocks"
+  },
+  { 
+    "chrisgrieser/nvim-spider", 
+    lazy = true,
+    -- dependencies = {
+    -- 	"theHamsta/nvim_rocks",
+    -- 	build = "/home/smmm/.config/nvim/venv/bin/pip3 install hererocks && /home/smmm/.config/nvim/venv/bin/python3 -mhererocks . -j2.1.0-beta3 -r3.0.0 && cp nvim_rocks.lua lua",
+    -- 	config = function() require("nvim_rocks").ensure_installed("luautf8") end,
+    -- },
+  },
   {
     "folke/flash.nvim",
     event = "VeryLazy",
@@ -266,7 +277,148 @@ require("lazy").setup({
         }
       })
     end,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    lazy = false,
+    config = function()
+      require("neo-tree").setup({
+        window = {
+          position = "float", 
+          mapping_options = {
+            noremap = true,
+            nowait = true,
+          },
+        mappings = {
+          ["<space>"] = {
+            "toggle_node",
+            nowait = false,
+          },
+          ["<2-LeftMouse>"] = "open",
+          ["<cr>"] = "open",
+          ["<esc>"] = "cancel", -- close preview or floating neo-tree window
+          ["P"] = {
+            "toggle_preview",
+            config = {
+              use_float = true,
+              use_snacks_image = true,
+              use_image_nvim = true,
+            },
+          },
+          -- Read `# Preview Mode` for more information
+          ["l"] = "focus_preview",
+          ["S"] = "open_split",
+          ["s"] = "open_vsplit",
+          -- ["S"] = "split_with_window_picker",
+          -- ["s"] = "vsplit_with_window_picker",
+          ["t"] = "open_tabnew",
+          -- ["<cr>"] = "open_drop",
+          -- ["t"] = "open_tab_drop",
+          ["w"] = "open_with_window_picker",
+          --["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
+          ["C"] = "close_node",
+          -- ['C'] = 'close_all_subnodes',
+          ["z"] = "close_all_nodes",
+          ["Z"] = "expand_all_nodes",
+          --["Z"] = "expand_all_subnodes",
+          ["a"] = {
+            "add",
+            -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
+            -- some commands may take optional config options, see `:h neo-tree-mappings` for details
+            config = {
+              show_path = "none", -- "none", "relative", "absolute"
+            },
+          },
+          ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+          ["d"] = "delete",
+          ["r"] = "rename",
+          ["b"] = "rename_basename",
+          ["y"] = "copy_to_clipboard",
+          ["x"] = "cut_to_clipboard",
+          ["p"] = "paste_from_clipboard",
+          ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
+          -- ["c"] = {
+          --  "copy",
+          --  config = {
+          --    show_path = "none" -- "none", "relative", "absolute"
+          --  }
+          --}
+          ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
+          ["q"] = "close_window",
+          ["R"] = "refresh",
+          ["?"] = "show_help",
+          ["<"] = "prev_source",
+          [">"] = "next_source",
+          ["i"] = "show_file_details",
+          -- ["i"] = {
+          --   "show_file_details",
+          --   -- format strings of the timestamps shown for date created and last modified (see `:h os.date()`)
+          --   -- both options accept a string or a function that takes in the date in seconds and returns a string to display
+          --   -- config = {
+          --   --   created_format = "%Y-%m-%d %I:%M %p",
+          --   --   modified_format = "relative", -- equivalent to the line below
+          --   --   modified_format = function(seconds) return require('neo-tree.utils').relative_date(seconds) end
+          --   -- }
+          -- },
+          },
+        },
+        filesystem = {
+          filtered_items = {
+            visible = false, -- when true, they will just be displayed differently than normal items
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_hidden = false, 
+          },
+        },
+        buffers = {
+          follow_current_file = {
+            enabled = true,
+            leave_dirs_open = false,
+          },
+          group_empty_dirs = true,
+          show_unloaded = true,
+          window = {
+            mappings = {
+              ["d"] = "buffer_delete",
+              ["bd"] = "buffer_delete",
+              ["<bs>"] = "navigate_up",
+              ["."] = "set_root",
+            },
+          },
+        },
+        git_status = {
+          window = {
+            position = "float",
+            mappings = {
+              ["A"] = "git_add_all",
+              ["gu"] = "git_unstage_file",
+              ["gU"] = "git_undo_last_commit",
+              ["ga"] = "git_add_file",
+              ["gr"] = "git_revert_file",
+              ["gc"] = "git_commit",
+              ["gp"] = "git_push",
+              ["gg"] = "git_commit_and_push",
+            },
+          },
+        },
+        close_if_last_window = false,
+        enable_git_status = true,
+        enable_diagnostics = true,
+        open_files_using_relative_paths = false,
+        sort_case_insensitive = false,
+        sort_function = nil,
+      })
+    end,
   }
 })
 vim.g.highlightedyank_highlight_duration = 300
 vim.cmd.colorscheme "catppuccin"
+vim.cmd("set keymap=russian-jcukenwin")
+vim.cmd("set iminsert=0")
+vim.cmd("set imsearch=0")
